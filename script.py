5e2ea806016c16c3eb518274cb4c0684047f967c
OCataldo <oliviacataldo@uchicago.edu>
2024-10-08 22:47:26 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,24 @@
-# This file is intentionally empty
-#
-# add your code for printing out a message, such as 'Hello World!' or something else!
+## PA 00
+# Olivia Cataldo
+
+## About Me
+- I am a junior from NJ, majoring in Psychology. Decided to minor in Computation Social Science... _maybe_?
+- I play softball for the University - I **LOVE** my team!
+- I love to bake, travel, ski, and play golf & tennis.
+- Sushi is definitely my favorite food.  
+
+## Pictures
+<img src="https://github.com/user-attachments/assets/e17cd0fe-7aa5-41f4-b176-66b81c08dfbb" width="200">
+
+Picture of me!
+
+<img src="https://github.com/user-attachments/assets/3ee95c6d-38f3-4688-b033-12846bb466e9" width=400>
+
+My team!
+
+## Link
+This summer I went to Kiawah for a week and here is a video from a fishing boat!
+[Kiawah Video](https://github.com/user-attachments/assets/82d687b4-b782-41f7-a1e3-fe11ef53bf45)
+
+## Summary/Reflection
+This was my first experience using any type of software/program so I relied a lot on articles and youtube videos. I was able to figure out the simpler things such as how to make a title larger to changing the name display of my URL. It was really exciting for me because the whole process was new but I got to break it down by section and tackle it one problem at a time! 
